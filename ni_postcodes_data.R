38830550e8465be90001084e10fdc7bb0724ec2d
Danny Regan <danny.regan@protonmail.com>
2020-04-05 17:52:52 +0100

    finished postcodes, writeup started

--- ni_postcodes_data.R
+++ ni_postcodes_data.R
@@ -1,28 +1,28 @@
-library(mice)
 library(VIM)
+library(tidyverse)
 # ----------------------------------------------
 # Functions for use with the NI Postcode dataset
 # ----------------------------------------------
 
-describe_data <- function(postcodes){
+describe_data <- function(dataframe){
   # a) 
   # Show the total number of rows
   cat("\n\n------------------\nTotal rows:\n------------------\n")
-  print(nrow(postcodes))
+  print(nrow(dataframe))
   # structure of the data frame
   cat("\n\n------------------\nDataframe structure:\n------------------\n")
-  str(postcodes)
+  str(dataframe, width = 80, strict.width = "cut")
   # First 10 rows of the data frame containing all of the NIPostcode data.
   cat("\n\n------------------\nFirst 10 rows:\n------------------\n")
-  head(postcodes, 10)
+  head(dataframe, 10)
 }
 
-rename_columns <- function(postcodes){
+rename_columns <- function(dataframe){
   # b) Add a suitable title for each attribute of the data.
   cat("\n\n------------------\nBefore update:\n------------------\n")
-  print(colnames(postcodes))
+  print(colnames(dataframe))
   
-  colnames(postcodes) <- c("Organisation Name",
+  colnames(dataframe) <- c("Organisation Name",
                            "Sub-building Name",
                            "Building Name", 
                            "Number",
@@ -39,33 +39,39 @@ rename_columns <- function(postcodes){
                            "Primary Key")
   
   cat("\n\n------------------\nAfter update:\n------------------\n")
-  print(colnames(postcodes))
-  return(postcodes)
+  print(colnames(dataframe))
+  return(dataframe)
 }
 
-
-
-show_missing_value_counts <- function(){
-  # d) Show the total number of missing values for each column in the 
-  # postcode data frame
-}
-
-move_primary_key <- function(){
+move_primary_key <- function(dataframe){
   # e) Move the primary key identifier to the start of the dataset.
+  cat("\n\n------------------\nColumns before reorder:\n------------------\n")
+  print(colnames(dataframe))
+  updated_postcodes <- dataframe %>%
+    select("Primary Key", everything())
+  cat("\n\n------------------\nColumns after reorder:\n------------------\n")
+  print(colnames(updated_postcodes))
+  return(updated_postcodes)
 }
 
-extract_limavady_data <- function(){
+extract_limavady_data <- function(dataframe){
   # f) Create a new dataset called Limavady_data. 
   # Store within it only information where locality, townland
   # and town contain the name Limavady. Count and display the number of rows.
   # Store this information in a csv file called Limavady
-}
-
-create_cleaned_csv <- function(){
-  # g) Save the modified NIPostcode dataset in a csv file called CleanNIPostcodeData
+  attach(dataframe)
+  # check whether the relevant columns contain "limavady" with grepl, ignoring case
+  limavady_data <- dataframe[(grepl('limavady', Locality, ignore.case = TRUE) | 
+                               grepl('limavady', Townland, ignore.case = TRUE) | 
+                               grepl('limavady', Town, ignore.case = TRUE)),]
+  detach(dataframe)
+  cat("\n\n------------------\nLimavady dataset row count:\n------------------\n")
+  print(nrow(limavady_data))
   
+  # write limavady data to csv
+  write_csv(limavady_data, "data/limavady.csv")
+  return(limavady_data)
 }
-
 
 # ----------------------------------------------
 # Execute code
@@ -85,7 +91,6 @@ ni_postcodes <- rename_columns(ni_postcodes)
 # Replacing these with NA to get a clearer idea of what is missing
 ni_postcodes[ni_postcodes == ""] <- NA
 
-md.pattern(ni_postcodes)
 missing_values <- aggr(ni_postcodes, 
                        prop = FALSE, 
                        numbers = TRUE, 
@@ -109,4 +114,15 @@ missing_values_after_removal <- aggr(ni_postcodes,
                                      plot = FALSE)
 missing_values_after_removal[["missings"]]
 # After the rows with NA for both Town and Postcode were removed 
-# The dataset now has records with Town missing in 19446 and Postcode missing in 8474 
+# The dataset now has 19446 records with Town missing in 19446 records with Postcode missing.
+cat("\n\n------------------\nTotal rows after cleaning:\n------------------\n")
+print(nrow(ni_postcodes))
+
+# move primary key to first column position
+ni_postcodes <- move_primary_key(ni_postcodes)
+
+# Extract limavady data from main dataframe and write to data/limavady.csv
+limavady_data <- extract_limavady_data(ni_postcodes)
+
+# g) Save the modified NIPostcode dataset in a csv file called CleanNIPostcodeData
+write_csv(ni_postcodes, "data/CleanNIPostcodeData.csv")
